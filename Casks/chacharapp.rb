49816ff351822bcb72711dfc94c47d6f00fb864c
JUAN PABLO CASTRO <juanpablo@castrosolutions.dev>
2026-07-03 10:16:17 +0200

    fix: mark download domain as verified in cask url

--- Casks/chacharapp.rb
+++ Casks/chacharapp.rb
@@ -2,7 +2,8 @@ cask "chacharapp" do
   version "1.1.5"
   sha256 "1ce7cc1213956b4c25dbe6b9e0fd67b6bba32214587dc2fc3191063f6c1575da"
 
-  url "https://dl.juanpablocastro.com/releases/#{version}/ChacharApp-#{version}.dmg"
+  url "https://dl.juanpablocastro.com/releases/#{version}/ChacharApp-#{version}.dmg",
+      verified: "dl.juanpablocastro.com/"
   name "ChacharApp"
   desc "Local, free, private voice dictation tuned for Spanish-English code-switching"
   homepage "https://github.com/castrosolutions/ChacharApp"
